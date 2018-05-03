528096ec439812296f0e35ab69a272513242de91
Watson <watson1978@gmail.com>
2018-05-04 03:55:19 +0900

    検索クエリーを変数化

--- sample2.rb
+++ sample2.rb
@@ -3,11 +3,13 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+search_query = "cat"
+
 # https://urashita.com/archives/8158 を参考にAPIキーと検索エンジンIDを設定してください。
 custom_search_api_key = "APIキー"
 search_engine_id = "検索エンジンID"
 
-uri = URI(URI.escape("https://www.googleapis.com/customsearch/v1?key=#{custom_search_api_key}&cx=#{search_engine_id}&q=cat&hl=ja&searchType=image&start=1"))
+uri = URI(URI.escape("https://www.googleapis.com/customsearch/v1?key=#{custom_search_api_key}&cx=#{search_engine_id}&q=#{search_query}&hl=ja&searchType=image&start=1"))
 res = Net::HTTP.get_response(uri)
 json = JSON.parse(res.body)
 
